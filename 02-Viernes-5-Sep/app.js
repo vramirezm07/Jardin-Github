5061883ae6ae36e2be0fd41429638021a8991bbd
kuaki <vramirezm@centro.edu.mx>
2025-09-05 20:56:28 -0600

    🌲¡Agrego: Añadi 5 conos para la estructura de mi árbol y un pequeño cilindro para el tronco! (Aprendi como funciona el modificar los tamaños, radios y bordes de las figuras)

---
+++ 02-Viernes-5-Sep/app.js
@@ -0,0 +1,99 @@
+console.log ("Sesion 05. Ejercicio 01: Geometrias.");
+console.log(THREE);
+
+
+//configurar canvas
+const canvas = document.getElementById("lienzo");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Creamos nuestros elementos basicos
+// Escena, cámara, mesh y render
+
+//Escena
+const scene = new THREE.Scene();
+//Cámara
+//const camera = new THREE.Camera(fov, SVGPreserveAspectRatio, NodeIterator,far);
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000);
+//Mesh 
+
+//Geometría                             /radio, altura, 32 segmentos
+const geometry01 = new THREE.ConeGeometry(1, 1, 32);
+const geometry02 = new THREE.ConeGeometry(1.3, 1.5, 32);
+const geometry03 = new THREE.ConeGeometry(1.5, 1.7, 32);
+const geometry04 = new THREE.ConeGeometry(1.7, 1.9, 32);
+const geometry05 = new THREE.ConeGeometry(2, 2.2, 32);
+
+//nCylinderGeometry(radiusTop, radiusBottom, height, radialSegments);
+const geometry06 = new THREE.CylinderGeometry(0.5, 0.7, 1, 32);
+
+// Material
+const material = new THREE.MeshPhongMaterial(
+   {flatShading: true, specular: "#ffffff", shininess: 100, color: "#009f40"}
+);
+
+const madera = new THREE.MeshPhongMaterial(
+   {flatShading: true, specular: "#ffffff", shininess: 100, color: "#511607"}
+);
+
+const mesh = new THREE.Mesh(geometry01, material);
+scene.add(mesh);
+//Posición del mesh
+mesh.position.z = -10;
+mesh.position.y = 2;
+
+const mesh2 = new THREE.Mesh(geometry02, material);
+scene.add(mesh2);
+mesh2.position.z = -10;
+mesh2.position.y = 1.7;
+
+const mesh3 = new THREE.Mesh(geometry03, material);
+scene.add(mesh3);
+mesh3.position.z = -10;
+mesh3.position.y = 1.3;
+
+const mesh4 = new THREE.Mesh(geometry04, material);
+scene.add(mesh4);
+mesh4.position.z = -10;
+mesh4.position.y = 0.9;
+
+const mesh5 = new THREE.Mesh(geometry05, material);
+scene.add(mesh5);
+mesh5.position.z = -10;
+mesh5.position.y = 0.5;
+
+
+const mesh6 = new THREE.Mesh(geometry06, madera);
+scene.add(mesh6);
+mesh6.position.z = -10;
+mesh6.position.y = -1;
+
+
+//Render {}-> objeto, llamar al constructor en este caso canvas
+const renderer = new THREE.WebGLRenderer({canvas: canvas});
+
+//Dar instrucciones de renderizado
+renderer.render(scene, camera);
+
+
+function animate() {
+   requestAnimationFrame(animate);
+
+   renderer.render(scene, camera);
+}
+animate();
+
+const topLight = new THREE.PointLight("#ffbb66", 100, 100); // luz cálida
+topLight.position.y = 5;
+scene.add(topLight);
+
+const frontLight = new THREE.PointLight("#ffd580", 10, 100); // luz cálida secundaria
+frontLight.position.set(3, 1, 3);
+scene.add(frontLight);
+
+// Luz ambiental cálida y suave
+const ambientLight = new THREE.AmbientLight("#ffe0b2", 0.4); // color cálido, intensidad baja
+scene.add(ambientLight);
+
+
+
