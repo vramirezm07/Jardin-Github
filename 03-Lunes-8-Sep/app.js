90d8b1bde2833cf7d4b1ff43c0996e878b6ec8f7
kuaki <vramirezm@centro.edu.mx>
2025-09-08 21:19:29 -0600

    ⭐Agrego: ellipse, lineas y trinagulo para la creacion de la parte inferior de mi cara (barba).

--- 03-Lunes-8-Sep/app.js
+++ 03-Lunes-8-Sep/app.js
@@ -1,4 +1,5 @@
 console.log("kandinsky");
+
 // 1. Obtener referencia del canvas
 const canvas = document.getElementById("lienzo");
 console.log(canvas); //verificar que se obtuvo correctamente
@@ -10,48 +11,50 @@ canvas.height=window.innerHeight;
 // 2. Definir si es 2D o 3D
 const ctx = canvas.getContext('2d'); 
 
-// 3. Definir los estilos de la linea
+//Linea referencia
 ctx.beginPath();
 ctx.lineWidth = 2;
-ctx.strokeStyle = "white";
-
-// 4. Definir los putnos que forman la linea
-    //ctx.moveTo(x,y);
-ctx.moveTo(190,144); //punto inicial
-ctx.lineTo(390,144); //punto final
-
-ctx.moveTo(190,244); 
-ctx.lineTo(390,244); 
-
-// 5. Renderizar la linea
-ctx.stroke();
-ctx.closePath();
-
-// 6. Definir los estilos del rectangulo
-ctx.beginPath();
 ctx.strokeStyle = "red";
-ctx.lineWidth = 4;
-ctx.fillStyle = "gray";
+ctx.moveTo(canvas.width/2,0); //punto inicial
+ctx.lineTo(canvas.width/2,canvas.height); //punto final
 
-// 7. Definir el rectangulo
-ctx.rect(450,244, 200, 100); //(x,y, ancho, alto)
+ctx.moveTo(0,canvas.height/2);
+ctx.lineTo(canvas.width,canvas.height/2);
 
-// 8. Renderizar el rectangulo
-ctx.fill();
 ctx.stroke();
 ctx.closePath();
 
 
-// Sintaxis elipse o circulo}
+// Barba
 ctx.beginPath();
-
-// 9. Estilos de la elipse
-ctx.fillStyle = "white";
-
-// 10. Definir la elipse
-ctx.ellipse(300, 400, 205, 205, 0, 0, Math.PI*0.7); //(x,y, radioX, radioY, rotacion, anguloInicio, anguloFinal)
-
-// 11. Renderizar la elipse
-ctx.fill();
+ctx.lineWidth= 1.5;
+ctx.strokeStyle= "black";
+ctx.ellipse(canvas.width/2 - 35, canvas.height/2, 35, 20, 0, 0, Math.PI*3); //(x,y, radioX, radioY, rotacion, anguloInicio, anguloFinal)
+ctx.stroke();
 ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 1.5;
+ctx.strokeStyle = "black";
+ctx.moveTo(canvas.width/2 - 75, canvas.height/2 - 25); //punto inicial
+ctx.lineTo(canvas.width/2 - 20 ,canvas.height/2 - 35); //punto final
+
+ctx.moveTo(canvas.width/2 - 55, canvas.height/2 - 28); 
+ctx.lineTo(canvas.width/2 - 15 ,canvas.height/2 - 30);
+
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 1.5;
+ctx.strokeStyle = "black";
+ctx.fillStyle = "black";
+ctx.moveTo(canvas.width/2 -60, canvas.height/2 + 40); //punto inicial
+ctx.lineTo(canvas.width/2, canvas.height/2+ 25);
+ctx.lineTo(canvas.width/2- 5, canvas.height/2 + 70);
+ctx.lineTo(canvas.width/2 -60, canvas.height/2 + 40);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
 
