fa42a5f6009c1bbff92f573c2fc6bcf894e5c58e
kuaki <vramirezm@centro.edu.mx>
2025-09-11 23:33:18 -0600

    🌱Creación: Index, CSS y JS

---
+++ 06-Jueves-11-Sep/app.js
@@ -0,0 +1,55 @@
+console.log("");
+
+// 1. Obtener referencia del canvas
+const canvas = document.getElementById("lienzo");
+console.log(canvas); //verificar que se obtuvo correctamente
+
+// 1.1. Sincroniar dimensiones del canvas virtual con el viewport
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
+
+// 2. Definir si es 2D o 3D
+const ctx = canvas.getContext('2d'); 
+
+
+  const width = canvas.width;
+  const height = canvas.height;
+  const distancia = 10;
+  const margin = 100; 
+  const lines = [];   // Arreglo para guardar propiedades de cada línea
+
+//líneas horizontales
+ for (let y = margin; y <= height - margin; y += distancia) {
+      lines.push({
+
+        y: y,
+        offset: Math.random() * Math.PI * 2, // desfase de tiempo único
+        color: `hsl(${Math.random() * 220}, 3%, 60%)`,
+        amplitude: Math.random() * 10 + 5,
+        speed: Math.random() * 0.01
+      });
+  }
+
+
+
+  function draw(t) {
+    ctx.clearRect(0, 0, width, height);
+    ctx.lineWidth = Math.random();
+
+    for (let line of lines) {
+      const wave = Math.sin(t * line.speed + line.offset) * line.amplitude;
+      const wave2 = Math.cos(t * line.speed + line.offset) * line.amplitude;
+
+
+      ctx.beginPath();
+      ctx.strokeStyle = line.color;
+      ctx.moveTo(0, line.y + wave);
+      ctx.lineTo(width, line.y + wave);
+
+      ctx.stroke();
+    }
+
+    requestAnimationFrame(draw);
+  }
+
+  requestAnimationFrame(draw);
