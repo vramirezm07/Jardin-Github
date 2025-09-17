5f9ead4206317e988d03090222f2fc6fd1032dd2
kuaki <vramirezm@centro.edu.mx>
2025-09-16 23:24:16 -0600

    ⭐Agrego: if + else para que sea aleatoreo el la "huida" del rectangulo

--- 09-Miercoles-16-Sep/app.js
+++ 09-Miercoles-16-Sep/app.js
@@ -9,11 +9,27 @@ window.addEventListener("mousedown", function(event) {
     const mouseX = event.clientX;
     const mouseY = event.clientY;
 
+
+    let px;
+    if (Math.random() < 0.5) {
+        px = gsap.utils.random(mouseX - 150, mouseX - gsap.utils.random(50)); 
+    } else {
+               px = gsap.utils.random(mouseX + 150 , mouseX + Math.random(50)); // segundo elemento como limite en este caso nuestra pantalla
+    }
+
+    let py;
+    if (Math.random() < 0.5) {
+        py = gsap.utils.random(mouseY - 150, mouseY - gsap.utils.random(50)); 
+    } else { 
+                py = gsap.utils.random(mouseY + 150, mouseY + Math.random(50),); // segundo elemento como limite en este caso nuestra pantalla
+    }
+
+
     gsap.to(
         ".rectangulo", 
             {
-                x: gsap.utils.random(mouseX, window.innerWidth), 
-                y: gsap.utils.random(mouseY, window.innerHeight),
+                x: px, 
+                y: py,
                 duration: 2, //segundos
                 ease:"power2.in",
                 onComplete: function() {
